829f9633f06e2f424a463933fc19f9d5c1cd8374
Andrés Felipe Gamboa Torres <andresfdgt@outlook.com>
2025-01-23 03:59:35 -0500

    Vista de inicio con detalle sencillos de la aplicación

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('core')
+
+@section('content')
+    <p class="h5">Se han creado formularios dentro de la aplicación para que puedan probarse fácilmente los endpoints. Solo es necesario navegar por el menú superior para acceder a ellos.
+        Si prefieren, también pueden utilizar Postman para hacer las pruebas.</p>
+
+@endsection
