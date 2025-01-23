35dd225216b0aeaf93c3217bfcd4199bfea3ac62
Andrés Felipe Gamboa Torres <andresfdgt@outlook.com>
2025-01-23 01:39:55 -0500

    Agregar rutas para el controlador de productos y el controlador de almacenes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\WarehouseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/warehouse', [WarehouseController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index']);
+
